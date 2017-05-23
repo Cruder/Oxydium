2fa5356dddd61bacdbfd85f3ceb23ca66903c810
alex-lairan <lairana@free.fr>
2017-05-23 10:23:54 +0200

    Add linked list for store variables

---
+++ src/linked_list/linked_list.c
@@ -0,0 +1,96 @@
+#include "linked_list.h"
+#include <stdio.h>
+#include <string.h>
+
+Node* nodeGetLast(Node* node);
+void  nodeDestroyRecursive(Node* node);
+bool  nodeContainsRecursive(Node* node, char* str);
+Node* nodeFindRecursive(Node* node, char* str);
+Node* nodeCreate(char* str, double value);
+void nodePrint(Node* node);
+
+LinkedList* linkedListCreate() {
+  LinkedList* list = (LinkedList*) malloc(sizeof(LinkedList));
+  list->size = 0;
+  list->head = NULL;
+
+  return list;
+}
+
+void linkedListDestroy(LinkedList* list) {
+  nodeDestroyRecursive(list->head);
+
+  free(list);
+  list = NULL;
+}
+
+void linkedListPut(LinkedList* list, char* str, double value) {
+  Node* last    = nodeGetLast(list->head);
+  Node* newNode = nodeCreate(str, value);
+
+  if(last == NULL) {
+    list->head = newNode;
+  } else {
+    last->next = newNode;
+  }
+
+  ++list->size;
+}
+
+bool linkedListContainsValue(LinkedList* list, char* str) {
+  return linkedListGet(list, str) == NULL ? false : true;
+}
+
+Node* linkedListGet(LinkedList* list, char* str) {
+  return nodeFindRecursive(list->head, str);
+}
+
+void linkedListPrint(LinkedList* list) {
+  printf("LinkedList [");
+  nodePrint(list->head);
+  printf("]\n");
+}
+
+void nodePrint(Node* node) {
+  if(node == NULL) { return; }
+
+  printf("{%s -> %lf}", node->name, node->value);
+  if(node->next != NULL) {
+    printf(", ");
+  }
+
+  nodePrint(node->next);
+}
+
+Node* nodeCreate(char* str, double value) {
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->name = (char*) malloc(sizeof(char) * (strlen(str) + 1));
+    strcpy(node->name, str);
+    node->value = value;
+    node->next  = NULL;
+    return node;
+}
+
+Node* nodeGetLast(Node* node) {
+  if(node       == NULL) { return NULL; }
+  if(node->next == NULL) { return node; }
+
+  return nodeGetLast(node->next);
+}
+
+void nodeDestroyRecursive(Node* node) {
+  if(node == NULL) { return; }
+
+  nodeDestroyRecursive(node->next);
+  free(node->name);
+  node->name = NULL;
+  free(node);
+  node = NULL;
+}
+
+Node* nodeFindRecursive(Node* node, char* str) {
+  if(node == NULL) { return NULL; }
+
+  return strcmp(str, node->name) == 0 ? node
+                                      : nodeFindRecursive(node->next, str);
+}
---
+++ src/linked_list/linked_list.h
@@ -0,0 +1,27 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+  struct Node* next;
+  char* name;
+  double value;
+} Node;
+
+typedef struct LinkedList {
+  Node* head;
+  size_t size;
+} LinkedList;
+
+LinkedList* linkedListCreate();
+void        linkedListDestroy(LinkedList* list);
+
+bool        linkedListContainsValue(LinkedList* list, char* str);
+void        linkedListPut(LinkedList* list, char* str, double value);
+Node*       linkedListGet(LinkedList* list, char* str);
+
+void        linkedListPrint(LinkedList* list);
+
+#endif /* end of include guard: __LINKED_LIST_H__ */
